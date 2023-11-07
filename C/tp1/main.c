f7cefcbd6d9aec86b97a877ccd485add7000412c
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2023-11-07 11:43:42 +0100

    first commit

---
+++ C/tp1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int x=10;
+    printf("enter a number\n");
+      scanf("%d",x);
+
+    printf("the number is %d.",x);
+    return 0;
+}
---
+++ java/HelloWorld.java
@@ -0,0 +1,50 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        // declaration d'un objet
+        Humain h1;
+        // instancier=> reserver de l'espace memoire pour un humain
+        h1 = new Humain();
+        
+        Humain h2 = new Humain();
+        
+        h1.prenom="mehdi";
+        h1.nom="tmimi";
+        h1.modifierAge(23);
+        int b=h1.lireAge();
+        System.out.println(b);
+        
+        h1.modifierAge(1000);
+        b=h1.lireAge();
+        System.out.println(b);
+        
+       /* h2.prenom="hamza";
+        h2.nom="berrada";
+      //  System.out.println(h1.prenom);
+      // System.out.println(h2.prenom);
+        h1.salut();
+        h2.salut();*/
+    }
+}
+class Humain{
+    public String nom;
+    public String prenom;
+    private int age;
+    
+    // lire l age
+    public int getAge(){
+        return age;
+    }
+    
+    // modifier l age
+    public void setAge(int a){
+        if(a>0 && a<120)
+            age=a;
+    }
+    
+    // visibility typeRetour nomFonction(params){
+        //contenu
+    //}
+    public void salut(){
+            System.out.println("salut "+nom);
+    }
+}
