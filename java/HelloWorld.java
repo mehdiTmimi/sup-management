26cb7f04b084c2e534e6cc50335c3a7a8b104b54
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2023-11-07 11:53:00 +0100

    ok

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -26,7 +26,7 @@ class HelloWorld {
     }
 }
 class Humain{
-    public String nom;
+    private String nom;
     public String prenom;
     private int age;
     
@@ -41,6 +41,20 @@ class Humain{
             age=a;
     }
     
+    public String getNom(){
+        return nom;
+    }
+    public void setNom(String a)
+    {
+        nom=a;
+    }
+     public String getPrenom(){
+        return prenom;
+    }
+    public void setPrenom(String a)
+    {
+        prenom=a;
+    }
     // visibility typeRetour nomFonction(params){
         //contenu
     //}
