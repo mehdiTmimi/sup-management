bc8766adc9e5de6b2411b007d343247f72f04f6c
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2023-11-09 13:22:54 +0100

    ok

--- C/tp1/main.c
+++ C/tp1/main.c
@@ -7,11 +7,13 @@ int resultat;
 
 
 int main(){ 
+    printf("bienvenue dans notre premiere application\n");
     printf("veuillez saisir un nombre: ");
     scanf("%d",&nbr1) ;
+    printf("merci\n");
     printf("veuillez saisir un autre nombre:");
     scanf("%d",&nbr2);
-
+    printf("merci\n");
     resultat = nbr1 * nbr2;
 
     printf("%d",resultat);
