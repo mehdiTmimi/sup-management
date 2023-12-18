ba960c8eb516c0798539f9a48139e89c78493518
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2023-12-18 09:56:00 +0100

    ok

--- tp1/main.py
+++ tp1/main.py
@@ -13,7 +13,8 @@ def menu():
     print(" 1 - Infos du Zoo")
     print(" 2 - Ajouter un animal au Zoo")
     print(" 3 - les cries des animaux")
-    print(" 4 - quitter")
+    print(" 4 - supprimer un animal")
+    print(" 5 - quitter")
     return int(input("entrer votre choix ( 1 à 4)"))
 def infoZoo():
     print(f"le {zoo['nom']} se trouve à {zoo['address']}")
@@ -30,8 +31,24 @@ def ajouterAnimal():
 def crieAnimaux():
     for animal in zoo["animaux"]:
         animal.crier()
+def deleteAnimal():
+    """
+    indice  = 0
+    for animal in zoo["animaux"]:
+        print(f"{indice} : {animal}")
+        indice = indice + 1 
+    """
+    for i, animal in enumerate(zoo["animaux"]):
+        # https://www.geeksforgeeks.org/iterate-over-a-list-in-python/
+        print(f"{i} : {animal}")
+    pos = int(input("donnez la position de l'animal : "))
+    zoo["animaux"].pop(pos)
+    print("animal est supprimé")
+    
 choix = 0
-while choix!=4:
+zoo["animaux"].append(Elephant("africain",1000))
+zoo["animaux"].append(Tigre("africain",200))
+while choix!=5:
     choix = menu()
     if choix==1:
         infoZoo()
@@ -40,6 +57,8 @@ while choix!=4:
     elif choix == 3 :
         crieAnimaux()
     elif choix == 4 :
+        deleteAnimal()
+    elif choix == 5 :
         print("Au revoir")
     else:
         print("choix incorrecte")
